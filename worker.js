2845aba40d25058cc9133810064d0d2c873c2901
stherlionato-cmd <stherlionato@gmail.com>
2026-05-05 18:27:00 -0300

    Atualizar o worker.js

--- worker.js
+++ worker.js
@@ -285,7 +285,7 @@ if(!valor){
 
 try{
 
-const apikey = config.tipo === "sara" ? "artigo%23171_b" : "bigmouthh";
+const apikey = config.tipo === "sara" ? "bigmouthh" : "bigmouthh";
 
 const apiURL = config.url + "?" +
   config.param + "=" + encodeURIComponent(valor) +
