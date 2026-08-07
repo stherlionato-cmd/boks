75184065f5a87c6660feb32019b74263bb74d783
stherlionato-cmd <stherlionato@gmail.com>
2026-08-07 05:31:09 -0300

    Atualizar o worker.js

--- worker.js
+++ worker.js
@@ -232,7 +232,7 @@ if(!valor){
 
 try{
 
-const apikey = config.tipo === "sara" ? "0b1toSpam" : "0b1toSpam";
+const apikey = config.tipo === "sara" ? "@0b1toSpam" : "@0b1toSpam";
 
 const apiURL = config.url + "?" +
   config.param + "=" + encodeURIComponent(valor) +
