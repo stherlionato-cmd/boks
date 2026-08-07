df2786da5c2b99b8d6386a0b11fdcef9d5ef7822
stherlionato-cmd <stherlionato@gmail.com>
2026-08-07 05:36:48 -0300

    Atualizar o worker.js

--- worker.js
+++ worker.js
@@ -6,11 +6,11 @@ let endpoint = url.pathname.replace("/","")
 
 // 🔥 ALIAS
 const ALIAS = {
-  cpf2:"cpf",
-  cpf3:"cpf",
-  cpf4:"cpf",
-  cpf5:"cpf",
-  cpf6:"cpf"
+  cpf2:"cpf2",
+  cpf3:"cpf3",
+  cpf4:"cpf4",
+  cpf5:"cpf5",
+  cpf6:"cpf6"
 }
 
 if(ALIAS[endpoint]){
