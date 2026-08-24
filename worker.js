2f591f36b78cb97e7165a97b66079a730d6be4fe
stherlionato-cmd <stherlionato@gmail.com>
2026-08-24 00:42:45 -0300

    Atualizar o worker.js

--- worker.js
+++ worker.js
@@ -232,7 +232,7 @@ if(!valor){
 
 try{
 
-const apikey = config.tipo === "sara" ? "KEY_utvz7pq5" : "KEY_utvz7pq5";
+const apikey = config.tipo === "sara" ? "KEY_qd5ceehn" : "KEY_qd5ceehn";
 
 const apiURL = config.url + "?" +
   config.param + "=" + encodeURIComponent(valor) +
